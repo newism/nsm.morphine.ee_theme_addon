f0c93da8493f58bdc21637128f177382e27d11dd
Leevi Graham <leevi@newism.com.au>
2010-07-07 11:52:20 +1000

    Updated alert styling

--- views/tables/simple.php
+++ views/tables/simple.php
@@ -70,6 +70,48 @@
 			</tbody>
 		</table>
 	</div>
+
+	<div class="tg">
+		<h2>Simple table group w/ alerts</h2>
+		<div class="alert info">A sample info alert</div>
+		<div class="alert error">A sample error alert</div>
+		<div class="alert warning">A sample warning alert</div>
+		<div class="alert success">A sample success alert</div>
+		<table>
+			<thead>
+				<tr>
+					<th scope="col">Heading 1</th>
+					<th scope="col">Heading 2</th>
+					<th scope="col">Heading 3</th>
+				</tr>
+			</thead>
+			<tfoot>
+				<tr>
+					<td>&nbsp;</td>
+					<td>Foot 2</td>
+					<td>Foot 3</td>
+				</tr>
+			</tfoot>
+			<tbody>
+				<tr class="even">
+					<th scope="row">Heading 1</th>
+					<td>td</td>
+					<td>td</td>
+				</tr>
+				<tr class="odd">
+					<th scope="row">Heading 2</th>
+					<td>td</td>
+					<td>td</td>
+				</tr>
+				<tr class="even">
+					<th scope="row">Heading 3</th>
+					<td>td</td>
+					<td>td</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
 	<div class="tg">
 		<h2>Data Table</h2>
 		<div class="alert info">Simple data table.</div>
@@ -232,3 +274,4 @@
 		</div>
 	</div>
 </div>
+
