95b0f0a2ca4f6b5e0ce8e9b5e2562f7f68371f11
Leevi Graham <leevi@newism.com.au>
2011-03-20 12:35:43 +1100

    Configuration for compass

---
+++ themes/third_party/nsm_morphine_theme/config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "styles"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "scripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+output_style = "compressed"
