f98d50d01e4d242d14f2e656eb7433b06bccd296
Leevi Graham <leevi@newism.com.au>
2011-03-26 17:50:00 +1100

    Load a compressed JS version

--- acc.nsm_morphine_theme.php
+++ acc.nsm_morphine_theme.php
@@ -33,21 +33,26 @@ class Nsm_morphine_theme_acc
 
 		$theme_folder_url = $EE->config->item('theme_folder_url');
 
-		if (substr($theme_folder_url, -1) != '/')
+		if (substr($theme_folder_url, -1) != '/') {
 			$theme_folder_url .= '/';
+		}
 
 		$theme_folder_url .= "third_party/nsm_morphine_theme/";
 
 		$EE->cp->add_to_head('<link rel="stylesheet" type="text/css" href="'.$theme_folder_url.'styles/screen.css" />');
 
+		/*
 		$js_libs = array(
 						'NSM_MagicCheckboxes', 'NSM_Stripeable', 'NSM_UpdateInputsOnChange',
 						'NSM_Cloneable', 'NSM_Navigation', 'NSM_AttributeAssigner',
 						'tablednd.0.5', 'behaviours'
 		);
 
-		foreach ($js_libs as $lib)
-    	    $EE->cp->add_to_head('<script type="text/javascript" charset="utf-8" src="'.$theme_folder_url.'scripts/jquery.'.$lib.'.js"></script>');
+		foreach ($js_libs as $lib) {
+			$EE->cp->add_to_head('<script type="text/javascript" charset="utf-8" src="'.$theme_folder_url.'scripts/jquery.'.$lib.'.js"></script>');
+		}
+		*/
+		$EE->cp->add_to_head('<script type="text/javascript" charset="utf-8" src="'.$theme_folder_url.'scripts/compressed.js"></script>');
 
 		$this->sections[] = '<script type="text/javascript" charset="utf-8">$("#accessoryTabs a.nsm_morphine_theme").parent().remove();</script>';
 	}
