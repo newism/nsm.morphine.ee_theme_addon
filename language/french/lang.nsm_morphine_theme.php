ee39184a5943f6050811db42787b32ab70c05d4e
Emmanuel Beauchamps <e.bops@hotmail.fr>
2012-10-21 18:23:27 +0200

    French language pack
    Translating the language pack into French

---
+++ language/french/lang.nsm_morphine_theme.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * NSM Morphine Theme Language File
+ *
+ * @package			NsmMorphineTheme
+ * @version			2.0.3
+ * @author			Leevi Graham <http://leevigraham.com>
+ * @copyright 		Copyright (c) 2007-2012 Newism <http://newism.com.au>
+ * @license 		Commercial - please see LICENSE file included with this distribution
+ * @link			http://expressionengine-addons.com/nsm-morphine-theme
+ * @see				http://expressionengine.com/public_beta/docs/development/modules.html#lang_file
+ */
+
+$lang = array(
+	'nsm_morphine_theme'
+	=> 'Thème NSM Morphine',
+
+	'nsm_morphine_theme_module_name'
+	=> 'Thème NSM Morphine',
+
+	'nsm_morphine_theme_module_description'
+	=> 'Test des éléments du thème',
+
+	// Index
+	'index_page_title'
+	=> 'Tests des styles de NSM Morphine',
+
+	'index_nav_title'
+	=> 'Tableau de bord',
+
+	// Alerts
+	'alerts_page_title'
+	=> 'Alertes',
+
+	'alerts_nav_title'
+	=> 'Alertes',
+
+	// Tables
+	'tables_page_title'
+	=> 'Tables',
+
+	'tables_nav_title'
+	=> 'Tables',
+
+	// Alerts
+	'buttons_page_title'
+	=> 'Boutons &amp; Icônes',
+
+	'buttons_nav_title'
+	=> 'Boutons &amp; Icônes',
+);
